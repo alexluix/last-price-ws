b4d53e2f527e0b97e8a4e009809359e35760895f
Alexander Land <landlab@gmail.com>
2018-07-21 21:08:03 +0300

    configured object mapper

--- src/main/java/pro/landlabs/pricing/App.java
+++ src/main/java/pro/landlabs/pricing/App.java
@@ -1,13 +1,24 @@
 package pro.landlabs.pricing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JodaModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
 }
